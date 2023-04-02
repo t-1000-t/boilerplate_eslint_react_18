fc6c267899efd6c62e0b0526b9f1e13cba4df962
vl <goognya@gmail.com>
2023-04-02 11:41:33 +0100

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div type='button'> Click </div>
+        !!
+      </header>
+    </div>
+  )
+}
+
+export default App
